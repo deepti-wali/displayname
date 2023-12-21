1a9f1effd428c67bd4c46d2755abb44c3a2b59af
Deepti <134826480+deepti-wali@users.noreply.github.com>
2023-12-21 22:47:05 +0530

    first commit

--- src/FullNameForm.js
+++ src/FullNameForm.js
@@ -1,6 +1,67 @@
 import React, { useState } from "react";
 import "./FullNameForm.css";
 
+// import React, { useState } from 'react';
+
+// const FullNameForm = () => {
+//   const [firstName, setFirstName] = useState("");
+//   const [lastName, setLastName] = useState("");
+//   const [fullName, setFullName] = useState("");
+//   const [error, setError] = useState("");
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     if (name === "firstName") {
+//       setFirstName(value);
+//     } else if (name === "lastName") {
+//       setLastName(value);
+//     }
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Concatenate names while handling special characters and numbers
+//     const sanitizedFirstName = firstName.replace(/[^a-zA-Z]/g, ""); // Allow only alphabets
+//     const sanitizedLastName = lastName.replace(/[^a-zA-Z]/g, ""); // Allow only alphabets
+//     const fullNameResult = `${sanitizedFirstName} ${sanitizedLastName}`;
+//     setFullName(fullNameResult);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="firstName">First Name:</label>
+//         <input
+//           type="text"
+//           id="firstName"
+//           name="firstName"
+//           value={firstName}
+//           onChange={handleInputChange}
+//           required
+//         />
+//         <br />
+//         <br />
+//         <label htmlFor="lastName">Last Name:</label>
+//         <input
+//           type="text"
+//           id="lastName"
+//           name="lastName"
+//           value={lastName}
+//           onChange={handleInputChange}
+//           required
+//         />
+//         <br />
+//         <br />
+//         <button type="submit">Submit</button>
+//       </form>
+
+//       {fullName && <div>Full Name: {fullName}</div>}
+//     </div>
+//   );
+// };
+
+// export default FullNameForm;
+
 const FullNameForm = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -26,7 +87,11 @@ const FullNameForm = () => {
       setError("Please use only letters in the first and last name fields.");
       setFullName("");
     } else {
-      const concatenatedName = `${firstName} ${lastName}`;
+      const sanitizedFirstName = firstName.replace(/[^a-zA-Z]/g, ""); // Allow only alphabets
+      const sanitizedLastName = lastName.replace(/[^a-zA-Z]/g, ""); // Allow only alphabets
+      //     const fullNameResult = `${sanitizedFirstName} ${sanitizedLastName}`;
+      //     setFullName(fullNameResult);
+      const concatenatedName = `${sanitizedFirstName} ${sanitizedLastName}`;
       setFullName(concatenatedName);
       setError("");
     }
